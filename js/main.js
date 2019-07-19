46f4c655a4b60d2452a089d1601c25693d404ddb
Shuvo Sarker <shuvooa707@gmail.com>
2019-07-19 20:37:11 +0600

    added isotope

--- js/main.js
+++ js/main.js
@@ -18,3 +18,29 @@ $(document).ready(function(){
         });
     });
 });
+
+
+ // Isotope
+ $('.iso-cont').isotope({
+    filter: '*',
+    animationOptions:{
+        easing:'easeIn',
+        duration:5000,
+        queue:false
+    }
+});
+
+// Sort & Filtering            
+$('.isotope-nav-item').click(function(){
+    $('.isotope-nav-item').removeClass('.current'); 
+    $('.isotope-nav-item').addClass('.current'); 
+    var selector = $(this).attr('data-filter');
+    $('.iso-cont').isotope({
+        filter: selector,
+        animationOptions:{
+            easing:'linear',
+            duration:1500,
+            queue:false
+        }
+    });   
+});
